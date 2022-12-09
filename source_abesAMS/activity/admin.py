6fd78681bd103d92bc0a73afe6758e0e905180d5
4the4ryushin <aman2001mi@gmail.com>
2022-12-10 01:39:42 +0530

    static , media files configurations accessbility code, forms views setup

--- source_abesAMS/activity/admin.py
+++ source_abesAMS/activity/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from activity.models import Activity, SubActivity
 
+
+
 admin.site.register(Activity)
 admin.site.register(SubActivity)
