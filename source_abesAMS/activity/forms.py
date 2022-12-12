dc196258233ec5897af13caed7ce8e0154cd11e3
4the4ryushin <aman2001mi@gmail.com>
2022-12-12 09:48:45 +0530

    forms updates

---
+++ source_abesAMS/activity/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+from . models import  Activity,SubActivity
+
+
+class ActivityForm(forms.ModelForm):
+	pass
---
+++ source_abesAMS/static/js/task_card.js
@@ -0,0 +1,4 @@
+function removeCard(el){
+	var element = el;
+	element.remove();	
+}
